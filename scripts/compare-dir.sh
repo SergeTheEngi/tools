c8e9a7567f4647d6f1319ab292078600a6f0b72d
jester <jester@localhost>
2024-08-31 11:34:20 +0000

    Update script to ignore the difference in paths

--- scripts/compare-dir.sh
+++ scripts/compare-dir.sh
@@ -4,5 +4,5 @@
 # Thanks Adail Junior (https://stackexchange.com/users/3625606/adail-junior)!
 
 compare-dir(){ # compare-dir <dir1> <dir2>
-   diff <(find "$1" -type f -exec md5sum {} + | sort -k 2) <(find "$2" -type f -exec md5sum {} + | sort -k 2)
+   diff <(find "$1" -type f -exec md5sum {} + | sort -k 2 | sed 's/ .*\// /') <(find "$2" -type f -exec md5sum {} + | sort -k 2 | sed 's/ .*\// /')
 }
