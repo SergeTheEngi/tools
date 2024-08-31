64d35667f3000b07e57501e59b328fa09b6415b0
jester <jester@localhost>
2024-08-31 09:59:48 +0000

    Add compare-dir.sh

---
+++ scripts/compare-dir.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# Recursively compares contents of two directories. Made to be loaded via bashrc or analogue.
+# Thanks Adail Junior (https://stackexchange.com/users/3625606/adail-junior)!
+
+compare-dir(){ # compare-dir <dir1> <dir2>
+   diff <(find "$1" -type f -exec md5sum {} + | sort -k 2) <(find "$2" -type f -exec md5sum {} + | sort -k 2)
+}
